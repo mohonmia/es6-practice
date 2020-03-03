e9bf0e52ab58f42e699f2d9dafe0d11d6839b393
mohonmia <mohonmia97@gmail.com>
2020-03-03 09:02:12 +0600

    es6 let and const

---
+++ letconst.js
@@ -0,0 +1,19 @@
+const name = "mohon mia";
+//name = "nazrul islam";
+console.log(name);
+const numbers = [24,42,442,42,55,1];
+numbers[3] = 33;
+numbers.push(12);
+console.log(numbers);
+
+let myName = "mohon";
+myName = "mohon mia";
+console.log(myName);
+
+let sum = 0;
+for(let i = 0;i < 10;i++){
+    sum = sum + i;
+}
+//console.log(i);  not work
+
+console.log(sum);
